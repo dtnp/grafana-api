ec4c1fd5f9f812f99cbd681af5c9df251af63523
Daniel Nitsch <daniel.nitsch@pantheon.io>
2024-05-24 17:21:20 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dtnp/go/grafana-api
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+)
+const (
+	grafanaUrl = "https://pantheon.grafana.net/api"
+)
+
+func main() {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	if err := run(log); err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(log *slog.Logger) error {
+	// ------------------------------------------------------------------------
+	// ENV Variables
+	// ------------------------------------------------------------------------
+	requiredEnvs := []string{"GRAFANA_TOKEN"}
+	errs := ""
+	for _, env := range requiredEnvs {
+		if os.Getenv(env) == "" {
+			errs += fmt.Sprintf("%s, ", env)
+		}
+	}
+
+	if errs != "" {
+		return errors.New(fmt.Sprintf("missing env variables: %s", strings.Trim(errs, " ,")))
+	}
+
+	// ------------------------------------------------------------------------
+	// CLI Args
+	// TODO: Currently required - make this optional
+	// ------------------------------------------------------------------------
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) < 1 {
+		return errors.New("missing args, please specify an api endpoint")
+	}
+	// ------------------------------------------------------------------------
+	// API Magics
+	// TODO:
+	// 	1. get all dashboards
+	// 	2. filter out any dev/scratch/whatever (make a list)
+	// 	3. loop through and pull out needed pieces (title, description, tags?)
+	// 	4. return all
+	// ------------------------------------------------------------------------
+
+	body, err := getDashboard(log, argsWithoutProg[0])
+	if err != nil {
+		return fmt.Errorf("getDashboards [%s]: %v", argsWithoutProg, err)
+	}
+	fmt.Println(body)
+
+	return nil
+}
+
+func getDashboard(log *slog.Logger, dashboardUID string) (string,error) {
+	// TODO: DON'T Fix - no one is going to use this in prod ... right?  RIGHT!?
+	// CLI Injection RISK!  YEA!!
+	url := fmt.Sprintf("%s/dashboards/uid/%s", grafanaUrl, dashboardUID)
+	// Inline debugging?  Damn skippy!
+	//fmt.Println(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("get request: %v", err)
+	}
+	bearer := "Bearer " + os.Getenv("GRAFANA_TOKEN")
+	req.Header.Add("Authorization", bearer)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("do request: %v", err)
+	}
+	defer res.Body.Close()
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return "", fmt.Errorf("read body: %v", err)
+	}
+
+	return string(body), nil
+}
