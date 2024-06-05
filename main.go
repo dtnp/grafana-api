4d681148620ff81f71187495a55a7d2d71c01cf6
Daniel Nitsch <daniel.nitsch@pantheon.io>
2024-06-05 17:34:21 -0400

    getting the message and searching through things... it all works

--- main.go
+++ main.go
@@ -14,6 +14,29 @@ const (
 	grafanaUrl = "https://pantheon.grafana.net/api"
 )
 
+var foldersToIgnore = [...]string {
+  "scratch",
+  "dev",
+}
+
+type dashboard struct {
+  ID int `json:"id"`
+  UID string `json:"uid"`
+  Title string `json:"title"`
+  Uri string `json:"uri"`
+  Url string `json:"url"`
+  Slug string `json:"slug"`
+  Type string `json:"type"`
+  Tags []string `json:"tags"`
+  //IsStarred bool `json:"isstarred"`
+  FolderId int `json:"folderid"`
+  FolderUid string `json:"folderuid"`
+  FolderTitle string `json:"foldertitle"`
+  FolderUrl string `json:"folderurl"`
+  //SortMeta int `json:"sortmeta"`
+  Description string `json:"description"`
+}
+
 func main() {
 	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
@@ -56,8 +79,6 @@ func run(log *slog.Logger) error {
 	// 	3. loop through and pull out needed pieces (title, description, tags?)
 	// 	4. return all
 	// ------------------------------------------------------------------------
-  var body string
-  var err error
   if argsWithoutProg[0] == "search" {
 
     queryParam := "%"
@@ -65,22 +86,44 @@ func run(log *slog.Logger) error {
     if len(argsWithoutProg) > 1 {
       queryParam = strings.TrimSpace(argsWithoutProg[1])
     }
-    body, err = getAllDashboards(queryParam)
+    allDashboards, err := getAllDashboards(queryParam)
     if err != nil {
       return fmt.Errorf("getAllDashboards: %v", err)
     }
+    //fmt.Println(allDashboards[0].Title)
+
+    pd,_ := parseDashboards(allDashboards)
+    j,_ := json.MarshalIndent(pd, "", " ")
+    fmt.Println(string(j))
 
   } else {
-    body, err = getDashboard(argsWithoutProg[0])
+    body, err := getDashboard(argsWithoutProg[0])
     if err != nil {
       return fmt.Errorf("getDashboards [%s]: %v", argsWithoutProg, err)
     }
-
-    // desc := parseDashboard(body)
+    fmt.Println(body)
   }
-  fmt.Println(body)
 
 	return nil
+}
+
+func parseDashboards(ad []dashboard) ([]dashboard, error) {
+  var filteredDashboards []dashboard
+  for _, d := range ad {
+    // skip folders
+    if d.Type == "dash-folder" {
+      continue
+    }
+
+    fmt.Println(d.Title)
+    singleDashboard,_ := getDashboard(d.UID)
+    desc := getDescription(singleDashboard)
+
+    d.Description = desc
+    filteredDashboards = append(filteredDashboards, d)
+  }
+
+  return filteredDashboards, nil
 }
 
 func getDashboard(dashboardUID string) (string,error) {
@@ -105,7 +148,7 @@ func getDashboard(dashboardUID string) (string,error) {
 		return "", fmt.Errorf("read body: %v", err)
 	}
 
-	return string(body), nil
+  return string(body), nil
 }
 
 // parseDashboard - technically, this is only pulling out a description for now
@@ -115,34 +158,39 @@ func getDescription(body string) string {
  
   // mUahhaha - this is fantastically gross looking
   dashboard := res["dashboard"]
-  desc := dashboard.(map[string]interface{})["description"].(string)
+  desc := dashboard.(map[string]interface{})["description"]
+  if desc == nil {
+    return ""
+  }
  
-  return desc
+  return desc.(string)
 }
 
-func getAllDashboards(queryParam string) (string, error) {
+func getAllDashboards(queryParam string) ([]dashboard, error) {
 
 	url := fmt.Sprintf("%s/search?query=%s", grafanaUrl, queryParam)
 	// Inline debugging?  Damn skippy!
 	//fmt.Println(url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", fmt.Errorf("get request: %v", err)
+		return nil, fmt.Errorf("get request: %v", err)
 	}
 	bearer := "Bearer " + os.Getenv("GRAFANA_TOKEN")
 	req.Header.Add("Authorization", bearer)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("do request: %v", err)
+		return nil, fmt.Errorf("do request: %v", err)
 	}
 	defer res.Body.Close()
 	body, readErr := io.ReadAll(res.Body)
 	if readErr != nil {
-		return "", fmt.Errorf("read body: %v", err)
+		return nil, fmt.Errorf("read body: %v", err)
 	}
 
+  var allDashboards []dashboard
+  json.Unmarshal(body, &allDashboards)
 
-  return string(body), nil
+  return allDashboards, nil
 }
 
 
